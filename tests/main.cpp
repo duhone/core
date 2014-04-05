dac1166bd41cc313d35ea14afd02304ec968c174
duhone <duhone@conjuredrealms.com>
2014-04-04 22:44:50 -0700

    unit test should pause if debugger is attached

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,7 +1,23 @@
 #include "gtest/gtest.h"
+#ifdef _MSC_VER
+#include <Windows.h>
+#endif
+#include <iostream>
 
+using namespace std;
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
 	::testing::InitGoogleTest(&argc, argv);
-	return RUN_ALL_TESTS();
+	auto result = RUN_ALL_TESTS();
+
+#ifdef _MSC_VER
+	if (IsDebuggerPresent())
+	{
+		cout << "Press return key to continue\n";
+		cin.get();
+	}
+#endif
+
+	return result;
 }
