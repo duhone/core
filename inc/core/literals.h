767def8f909c94f4760555d8e38bdfe810687c3d
duhone <duhone@conjuredrealms.com>
2015-08-09 13:12:34 -0700

    add byte size literals

---
+++ inc/core/literals.h
@@ -0,0 +1,23 @@
+#pragma once
+
+namespace CR
+{
+	namespace Core
+	{
+		namespace Literals
+		{
+			constexpr unsigned long long operator"" _Kb(unsigned long long a_bytes)
+			{
+				return a_bytes*1024;
+			}
+			constexpr unsigned long long operator"" _Mb(unsigned long long a_bytes)
+			{
+				return a_bytes * 1024 * 1024;
+			}
+			constexpr unsigned long long operator"" _Gb(unsigned long long a_bytes)
+			{
+				return a_bytes * 1024 * 1024 * 1024;
+			}
+		}
+	}
+}
