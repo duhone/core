040f7dc9942f8274c94b5299078414a4657effa7
duhone <duhone@outlook.com>
2021-03-27 20:34:38 -0700

    more cleanup on Table

--- inc/core/Table.h
+++ inc/core/Table.h
@@ -13,24 +13,24 @@
 #include <utility>
 
 namespace CR::Core {
-	// t_row... is a struct holding the data for  row. t_row... should not contain the primary key.
-	// You can have more than one t_row struct. This allows creating a SOAOS layout. The primary key is in one array,
-	// and each row in in an array. You can iterate just one row, or a multiple of rows at once.
-	// The fastest way to get to a row is by index, not by primary key, you can save these indices for future use. In
-	// general indices are stable. The function compact will invalidate them however, there is rarely a need to call
-	// compact.
-	template<typename t_primaryKey, typename... t_rows>
+	// t_column... is a struct(or primitive) holding the data for one column of a row. t_column... should not contain
+	// the primary key. You can have more than one t_column type. This allows creating a SOAOS layout. The primary key
+	// is in one array, and each column in an array. You can iterate the rows of the table for just one column. You can
+	// grab multiple iterators if you need to walk multiple columns at once. The fastest way to get to a row for a
+	// column is by index, not by primary key, you can save these indices for future use. In general indices are stable.
+	// The function compact will invalidate them however, there is rarely a need to call compact.
+	template<typename t_primaryKey, typename... t_columns>
 	class Table {
-		static_assert(is_unique_v<t_rows...>, "row types must be unique currently");
+		static_assert(is_unique_v<t_columns...>, "column types must be unique currently");
 
 		// Using arrays currently, and std::bitset. For larger tables need a bitset replacement(which would help here
 		// too). And should use std::vector instead of arrays for larger tables.
 		static constexpr uint16_t c_maxSize = 512;
-		// If row is larger than this, should refactor. Bulk data should be stored elsewhere on the heap(i.e.
+		// If column is larger than this, should refactor. Bulk data should be stored elsewhere on the heap(i.e.
 		// unique_ptr, or a vector). If still larger than this, then you probably aren't in boyce-codd normal form. Or
-		// you just need to split up the table.
+		// you just need to split up the table. You can of course have multiple columns.
 		static constexpr uint32_t c_maxRowSize = 64;
-		static_assert((... && (sizeof(t_rows) <= c_maxRowSize)));
+		static_assert((... && (sizeof(t_columns) <= c_maxRowSize)));
 
 	  public:
 		// A system wide constant, need to find a better place for it. Its duplicated in many public interfaces
@@ -46,40 +46,40 @@ namespace CR::Core {
 		Table& operator=(Table&&) = delete;
 
 		// returns the new index, fatal error if table is full
-		uint16_t CreateRow(t_primaryKey&& a_key, t_rows&&... a_row);
+		uint16_t insert(t_primaryKey&& a_key, t_columns&&... a_row);
 		// row will be default constructed if not standard layout, otherwise uninitialized
-		uint16_t CreateRow(t_primaryKey&& a_key);
+		uint16_t insert(t_primaryKey&& a_key);
 
 		// problematic when t_primaryKey is a string type(char*, string, string_view, ect) should just take a
 		// string_view for those. Wait until C++20 to fix, can be done cleaner there.
 		[[nodiscard]] uint16_t GetIndex(const t_primaryKey& a_key) const;
 
-		template<typename t_row>
-		[[nodiscard]] const t_row& GetRow(uint16_t a_index) const;
-		template<typename t_row>
-		[[nodiscard]] t_row& GetRow(uint16_t a_index);
+		template<typename t_column>
+		[[nodiscard]] const t_column& GetColumn(uint16_t a_index) const;
+		template<typename t_column>
+		[[nodiscard]] t_column& GetColumn(uint16_t a_index);
 
-		void DeleteRow(uint16_t a_index);
+		void erase(uint16_t a_index);
 
 	  private:
 		// returns c_maxSize if couldn't find one
 		[[nodiscard]] uint16_t FindUnused();
 
-		template<size_t index, typename t_row>
-		void ClearRow(uint16_t a_index, t_row&& a_row);
+		template<size_t index, typename t_column>
+		void ClearColumn(uint16_t a_index, t_column&& a_column);
 		template<size_t... tupleIndices>
-		void ClearRows(uint16_t a_index, std::index_sequence<tupleIndices...>, t_rows&&... rows);
+		void ClearRow(uint16_t a_index, std::index_sequence<tupleIndices...>, t_columns&&... row);
 
 		std::string m_tableName;
 		std::bitset<c_maxSize> m_used;
 		t_primaryKey m_primaryKeys[c_maxSize];
 		tsl::robin_map<t_primaryKey, uint16_t> m_lookUp;
-		std::tuple<std::array<t_rows, c_maxSize>...> m_rows;
+		std::tuple<std::array<t_columns, c_maxSize>...> m_rows;
 	};
 }    // namespace CR::Core
 
-template<typename t_primaryKey, typename... t_rows>
-inline uint16_t CR::Core::Table<t_primaryKey, t_rows...>::FindUnused() {
+template<typename t_primaryKey, typename... t_columns>
+inline uint16_t CR::Core::Table<t_primaryKey, t_columns...>::FindUnused() {
 	// TODO: std::bitset sucks, unless its 64 bits or less, can't do a bitscan manually even.
 	for(uint16_t i = 0; i < c_maxSize; ++i) {
 		if(!m_used[i]) { return i; }
@@ -87,31 +87,34 @@ inline uint16_t CR::Core::Table<t_primaryKey, t_rows...>::FindUnused() {
 	return c_maxSize;
 }
 
-template<typename t_primaryKey, typename... t_rows>
-template<size_t index, typename t_row>
-void CR::Core::Table<t_primaryKey, t_rows...>::ClearRow([[maybe_unused]] uint16_t a_index,
-                                                        [[maybe_unused]] t_row&& a_row) {
-	if constexpr(!std::is_standard_layout_v<t_row>) { std::get<index>(m_rows)[a_index] = std::move(a_row); }
+template<typename t_primaryKey, typename... t_columns>
+template<size_t index, typename t_column>
+void CR::Core::Table<t_primaryKey, t_columns...>::ClearColumn([[maybe_unused]] uint16_t a_index,
+                                                              [[maybe_unused]] t_column&& a_column) {
+	if constexpr(!std::is_standard_layout_v<t_column>) { std::get<index>(m_rows)[a_index] = std::move(a_column); }
 }
 
-template<typename t_primaryKey, typename... t_rows>
+template<typename t_primaryKey, typename... t_columns>
 template<size_t... tupleIndices>
-void CR::Core::Table<t_primaryKey, t_rows...>::ClearRows(uint16_t a_index, std::index_sequence<tupleIndices...>,
-                                                         t_rows&&... a_rows) {
-	(ClearRow<tupleIndices>(a_index, std::move(a_rows)), ...);
+void CR::Core::Table<t_primaryKey, t_columns...>::ClearRow(uint16_t a_index, std::index_sequence<tupleIndices...>,
+                                                           t_columns&&... a_row) {
+	(ClearColumn<tupleIndices>(a_index, std::move(a_row)), ...);
 }
 
-template<typename t_primaryKey, typename... t_rows>
-inline uint16_t CR::Core::Table<t_primaryKey, t_rows...>::CreateRow(t_primaryKey&& a_key, t_rows&&... a_rows) {
-	uint16_t unusedIndex = CreateRow(a_key);
+template<typename t_primaryKey, typename... t_columns>
+inline uint16_t CR::Core::Table<t_primaryKey, t_columns...>::insert(t_primaryKey&& a_key, t_columns&&... a_row) {
+	uint16_t unusedIndex = insert(a_key);
 
-	ClearRows(unusedIndex, std::index_sequence_for<t_rows...>{}, std::move(a_rows)...);
+	ClearRow(unusedIndex, std::index_sequence_for<t_rows...>{}, std::move(a_row)...);
 
 	return unusedIndex;
 }
 
-template<typename t_primaryKey, typename... t_rows>
-inline uint16_t CR::Core::Table<t_primaryKey, t_rows...>::CreateRow(t_primaryKey&& a_key) {
+template<typename t_primaryKey, typename... t_columns>
+inline uint16_t CR::Core::Table<t_primaryKey, t_columns...>::insert(t_primaryKey&& a_key) {
+	Log::Require(m_lookUp.find(a_key) == std::end(m_lookUp), "Tried to insert, but row already exists with this key {}",
+	             m_tableName);
+
 	uint16_t unusedIndex = FindUnused();
 	Log::Require(unusedIndex != c_maxSize, "Ran out of available rows in table {}", m_tableName);
 
@@ -123,8 +126,8 @@ inline uint16_t CR::Core::Table<t_primaryKey, t_rows...>::CreateRow(t_primaryKey
 	return unusedIndex;
 }
 
-template<typename t_primaryKey, typename... t_rows>
-inline uint16_t CR::Core::Table<t_primaryKey, t_rows...>::GetIndex(const t_primaryKey& a_key) const {
+template<typename t_primaryKey, typename... t_columns>
+inline uint16_t CR::Core::Table<t_primaryKey, t_columns...>::GetIndex(const t_primaryKey& a_key) const {
 	auto iter = m_lookUp.find(a_key);
 	if(iter == m_lookUp.end()) {
 		return c_unused;
@@ -133,24 +136,24 @@ inline uint16_t CR::Core::Table<t_primaryKey, t_rows...>::GetIndex(const t_prima
 	}
 }
 
-template<typename t_primaryKey, typename... t_rows>
+template<typename t_primaryKey, typename... t_columns>
 template<typename t_row>
-inline const t_row& CR::Core::Table<t_primaryKey, t_rows...>::GetRow(uint16_t a_index) const {
+inline const t_row& CR::Core::Table<t_primaryKey, t_columns...>::GetColumn(uint16_t a_index) const {
 	Log::Assert(a_index != c_unused && m_used[a_index], "asked for an unused row in table {}", m_tableName);
 
 	return std::get<std::array<t_row, c_maxSize>>(m_rows)[a_index];
 }
 
-template<typename t_primaryKey, typename... t_rows>
+template<typename t_primaryKey, typename... t_columns>
 template<typename t_row>
-inline t_row& CR::Core::Table<t_primaryKey, t_rows...>::GetRow(uint16_t a_index) {
+inline t_row& CR::Core::Table<t_primaryKey, t_columns...>::GetColumn(uint16_t a_index) {
 	Log::Assert(a_index != c_unused && m_used[a_index], "asked for an unused row in table {}", m_tableName);
 
 	return std::get<std::array<t_row, c_maxSize>>(m_rows)[a_index];
 }
 
-template<typename t_primaryKey, typename... t_rows>
-inline void CR::Core::Table<t_primaryKey, t_rows...>::DeleteRow(uint16_t a_index) {
+template<typename t_primaryKey, typename... t_columns>
+inline void CR::Core::Table<t_primaryKey, t_columns...>::erase(uint16_t a_index) {
 	Log::Assert(a_index != c_unused && m_used[a_index], "tried to delete an unused row in table {}", m_tableName);
 
 	m_used[a_index] = false;
@@ -158,5 +161,5 @@ inline void CR::Core::Table<t_primaryKey, t_rows...>::DeleteRow(uint16_t a_index
 	// To save on memory, if not a standard layout, then clear out the data. Hopefully if not standard layout
 	// then a proper move assignment was written.
 	if constexpr(!std::is_standard_layout_v<t_primaryKey>) { m_primaryKeys[a_index] = t_primaryKey{}; }
-	ClearRows(a_index, std::index_sequence_for<t_rows...>{}, t_rows{}...);
+	ClearRow(a_index, std::index_sequence_for<t_columns...>{}, t_columns{}...);
 }
--- tests/Table.cpp
+++ tests/Table.cpp
@@ -5,35 +5,35 @@
 #include <type_traits>
 
 namespace {
-	struct Row {
+	struct Column {
 		inline static int numConstructed{0};
 		inline static int numDestructed{0};
-		Row() { numConstructed++; }
-		~Row() { numDestructed++; }
+		Column() { numConstructed++; }
+		~Column() { numDestructed++; }
 
 		uint32_t foo1{0};
 		uint32_t foo2{1};
 	};
-	static_assert(std::is_standard_layout_v<Row>);
+	static_assert(std::is_standard_layout_v<Column>);
 }    // namespace
 
 TEST_CASE("table_simple") {
-	using t_Table = CR::Core::Table<uint32_t, Row>;
+	using t_Table = CR::Core::Table<uint32_t, Column>;
 	t_Table table{"simple table"};
 
 	REQUIRE(table.GetIndex(0) == t_Table::c_unused);
 
-	int preConstructed = Row::numConstructed;
-	int preDestructed  = Row::numDestructed;
+	int preConstructed = Column::numConstructed;
+	int preDestructed  = Column::numDestructed;
 
-	[[maybe_unused]] uint16_t index = table.CreateRow(0);
+	[[maybe_unused]] uint16_t index = table.insert(0);
 	REQUIRE(table.GetIndex(0) != t_Table::c_unused);
-	REQUIRE(preConstructed == Row::numConstructed);
-	REQUIRE(preDestructed == Row::numDestructed);
+	REQUIRE(preConstructed == Column::numConstructed);
+	REQUIRE(preDestructed == Column::numDestructed);
 
-	auto& row = table.GetRow<Row>(index);
+	auto& row = table.GetColumn<Column>(index);
 	row.foo1  = 2;
 	row.foo2  = 100;
 
-	table.DeleteRow(index);
+	table.erase(index);
 }
